ca6a995948823124f6932f3f246746242b4e2d8b
Rodolfo <rqm@outlook.es>
2021-11-17 22:22:04 -0600

    frases

---
+++ fraces/src/App.js
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+
+
+const Contenedor = styled.div`
+display: flex;
+align-items: center;
+padding-top: 5rem;
+flex-direction: column;
+`;
+
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+`;
+
+
+function App() {
+
+  const conSultarAPI = async () => {
+    const api = await fetch(`https://breakingbadapi.com/api/quote/random`)
+    const frase = await api.json();
+    console.log(frase[0]);
+  };
+
+  return (
+    <Contenedor>
+      <Boton
+        onClick={() => conSultarAPI()}
+      >Obtener Frace</Boton>
+    </Contenedor >
+
+  );
+}
+
+export default App;
---
+++ fraces/src/components/Contenedor.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from 'react';
+
+const Contenedor = () => {
+
+    //const [state, setstate] = useState(initialState);
+
+    return (
+
+        <Fragment>
+
+        </Fragment>
+
+    );
+
+};
+
+export default Contenedor;
